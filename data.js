527eeb4ae83b857540f064169bf2157dc33e38e1
Nicolly Carias da Silva <CIANDT\nicolly@lnb025657spo.ciandt.global>
2021-02-22 09:28:15 -0300

    [VBTB 3233] Resoluçao final

--- data.js
+++ data.js
@@ -1,9 +1,22 @@
 async function fetchData() {
   const response = await fetch('https://602a681d6c995100176ee4c4.mockapi.io/todos')
   const data = await response.json();
-  console.log(data)
 
-    function crescente(array) {
+  function removerOPadrao() {
+    const filho1 = document.getElementById('1');
+    const filho2 = document.getElementById('2');
+    const filho3 = document.getElementById('3');
+    const filho4 = document.getElementById('4');
+    const filho5 = document.getElementById('5');
+    filho1.parentNode.removeChild(filho1);
+    filho2.parentNode.removeChild(filho2);
+    filho3.parentNode.removeChild(filho3);
+    filho4.parentNode.removeChild(filho4);
+    filho5.parentNode.removeChild(filho5);
+  }
+  removerOPadrao();
+
+  function crescente(array) {
     for(let i = 0; i < array.length; i += 1) {
       for(let j = 0; j < array.length - 1; j+=1){
         let swap = [];
@@ -11,15 +24,31 @@ async function fetchData() {
           swap = array[j];
           array[j] = array[j+1]
           array[j+1] = swap;
-        }
+        };
       }
     }
     return array;
-  }
+  };
 
-  crescente(data)
+  crescente(data);
 
-  async function listarTasks(data) {
+  function ordenarData(array) {
+    for(let i = 0; i < array.length; i += 1) {
+      for(let j = 0; j < array.length - 1; j+=1){
+        let swap = [];
+        if(array[j].due_date > array[j+1].due_date && array[j].priority === array[j+1].priority) {
+          swap = array[j];
+          array[j] = array[j+1]
+          array[j+1] = swap;
+        };
+      };
+    };
+    return array;
+  };
+
+  ordenarData(data);
+
+  function listarTasks(data) {
     for (let i = 0; i < data.length; i += 1) {
       const parent = document.querySelector('#list');
       if(data[i].priority === 1) {
@@ -118,31 +147,13 @@ async function fetchData() {
 
   listarTasks(data);
 
-  function removerOPadrao() {
-    const filho1 = document.getElementById('1');
-    const filho2 = document.getElementById('2');
-    const filho3 = document.getElementById('3');
-    const filho4 = document.getElementById('4');
-    const filho5 = document.getElementById('5');
-    filho1.parentNode.removeChild(filho1);
-    filho2.parentNode.removeChild(filho2);
-    filho3.parentNode.removeChild(filho3);
-    filho4.parentNode.removeChild(filho4);
-    filho5.parentNode.removeChild(filho5);
-  }
-
-  removerOPadrao();
-
-   // function formatarData(data) {
+  // function formatarData(data) {
   //   for(let i = 0; i < data.length; i += 1) {
-  //     let string = data[i].due_date;
-  //     let converterParaDate = new Date(string).toLocaleDateString("pt-BR");
   //     // let dataFormatada = date[i].due_date.substr(0, 10).split('-');
   //     // let dataRevertida = dataFormatada.reverse().join('/');
-  //     return converterParaDate;
+  //     return dataRevertida;
   //   }
   // };
-  
 };
 
 fetchData();
